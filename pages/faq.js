93ed66e88532845ca5f71b6dabd136b892707124
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-11-29 08:50:37 -0300

    final da aula

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <a href="/">Ir para a home</a>
+        </div>
+    )
+}
