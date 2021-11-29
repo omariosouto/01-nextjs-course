e401d8ea7b088070f4e97d14d8eb1d22229350d0
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-11-29 09:04:53 -0300

    a1.4

---
+++ src/components/Link/index.js
@@ -0,0 +1,9 @@
+import NextLink from 'next/link';
+
+export default function Link({ children, href, ...props}) {
+    return (
+        <NextLink href={href} passHref>
+            <a {...props}>{children}</a>
+        </NextLink>
+    );
+}
