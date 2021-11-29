fa3d2ebc00711e7ce5bcf4b113f5a3d20ea080cd
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-11-29 09:19:15 -0300

    A1.5

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from '../src/components/Link';
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <h1>Alura Cases - Home</h1>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
