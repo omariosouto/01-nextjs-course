3392a63eb9189fa7047a7c093959ae1fc2d3a295
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-12-04 16:34:44 -0300

    final 2.1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import Link from '../src/components/Link';
 
+function Title({ children, as }) {
+    const Tag = as;
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                }
+            `}</style>
+        </React.Fragment>
+    );
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title as="h2">Alura Cases - Home</Title>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
