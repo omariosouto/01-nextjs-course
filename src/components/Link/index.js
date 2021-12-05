8e29bfac1f45405784a9cf5ab6a4f03b2c10d4d8
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-12-05 20:36:40 -0300

    update

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,9 +1,10 @@
 import NextLink from 'next/link';
+import { Text } from '../../theme/components';
 
 export default function Link({ children, href, ...props}) {
     return (
         <NextLink href={href} passHref>
-            <a {...props}>{children}</a>
+            <Text as="a" {...props}>{children}</Text>
         </NextLink>
     );
 }
