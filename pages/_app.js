393634687d05aeb0fac8cd0e7dd3d3b3c0929c54
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-12-06 21:21:09 -0300

    A4.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
 import Head from 'next/head';
+
 import GlobalStyle from '../src/theme/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-2QZDPX3BLK"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-2QZDPX3BLK');
+                        `
+                    }}
+                />
+
+
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
